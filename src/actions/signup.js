0154c4cb4649a2d9a740166717a421d9a92a2efe
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-24 15:25:05 +0530

    created signup action creators

---
+++ src/actions/signup.js
@@ -0,0 +1,39 @@
+import { myFirebase } from '../firebase/firebaseConfig';
+
+export const SIGNUP_REQUEST = "SIGNUP_REQUEST";
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const SIGNUP_FAILURE = "SIGNUP_FAILURE";
+
+
+const requestSignup = () => {
+    return {
+        type: SIGNUP_REQUEST
+    };
+};
+
+const receiveSignup = user => {
+    return {
+        type: SIGNUP_SUCCESS,
+        user
+    };
+};
+
+const signupError = () => {
+    return {
+        type: SIGNUP_FAILURE
+    }
+}
+
+
+// thunk for user login
+export const signupUser = (email, password) => dispatch => {
+    dispatch(requestSignup());
+    myFirebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(user => {
+            dispatch(receiveSignup(user));
+        })
+        .catch(error => {
+            dispatch(signupError());
+        })
+}
+
