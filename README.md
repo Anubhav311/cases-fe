3a275d9a6ed55c501445eecad6a7d63ff12d7184
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-19 15:56:29 +0530

    did some cleaning

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cases-fe
+
+
+links:
+https://itnext.io/firebase-login-functionality-from-scratch-with-react-redux-2bf316e5820f
+https://tylermcginnis.com/react-router-protected-routes-authentication/
+https://dev.to/dance2die/page-not-found-on-netlify-with-react-router-58mc
+https://dev.to/chrisachard/dns-record-crash-course-for-web-developers-35hn
--- src/App.js
+++ src/App.js
@@ -11,75 +11,28 @@ import CaseList from './components/caseList';
 import CaseForm from './components/caseForm';
 
 
-// const firebase = require("firebase");
-// Required for side-effects
-// require("firebase/firestore");
-
-// Initialize Cloud Firestore through Firebase
-// firebase.initializeApp({
-//     apiKey: 'AIzaSyByte5nh7cajduAOegw-DmqRupfHQWnyfk',
-//     authDomain: 'test-frbs-6650b.firebaseapp.com',
-//     projectId: 'test-frbs-6650b'
-// });
-
-// var db = firebase.firestore();
-
-// db.collection("users").add({
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-// })
-// .then(function(docRef) {
-//     console.log("Document written with ID: ", docRef.id);
-// })
-// .catch(function(error) {
-//     console.error("Error adding document: ", error);
-// });
-
-// Add a second document with a generated ID.
-// db.collection("users").add({
-//   first: "Alan",
-//   middle: "Mathison",
-//   last: "Turing",
-//   born: 1912
-// })
-// .then(function(docRef) {
-//   console.log("Document written with ID: ", docRef.id);
-// })
-// .catch(function(error) {
-//   console.error("Error adding document: ", error);
-// });
-
-// //get data
-// let dataVariable = []
-// db.collection("users").get().then((querySnapshot) => {
-//   querySnapshot.forEach((doc) => {
-//     dataVariable.push(doc.data())
-//       console.log(dataVariable[0]);
-//   });
-// });
-
-
 function App(props) {
 
   const { isAuthenticated, isVerifying } = props;
 
   return (
-    <Switch>
-      <div className="App">
-        <ProtectedRoute 
-          exact
-          path='/'
-          component={Home}
-          isAuthenticated={isAuthenticated}
-          isVerifying={isVerifying}
-        />
-        <Route path='/login' component={Login} />
-        {/* <CaseForm/>
-        <CaseList/> */}
-      <Signup />
-      </div>
-    </Switch>
+    <div>
+      <CaseForm/>
+      <CaseList/>      
+    </div>
+    // <Switch>
+    //   <div className="App">
+    //     <ProtectedRoute 
+    //       exact
+    //       path='/'
+    //       component={Home}
+    //       isAuthenticated={isAuthenticated}
+    //       isVerifying={isVerifying}
+    //     />
+    //     <Route path='/login' component={Login} />
+    //   <Signup />
+    //   </div>
+    // </Switch>
   );
 }
 
