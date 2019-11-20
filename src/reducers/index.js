741a9599996d88e70b5de1e6bbcc0beaa017a267
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-20 21:26:40 +0530

    added parts reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
-import casesReducer from './casesReducer'
+import casesReducer from './casesReducer';
+import partsReducer from './partsReducer';
 import auth from './auth';
 import signup from './signup'
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     cases: casesReducer,
+    parts: partsReducer,
     signup: signup,
     auth: auth
 })
