4f356615ff276204119df7abc1812df23c58d2bc
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-20 21:14:02 +0530

    created actioexport const NEW_CASE = 'NEW_CASE';

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,7 @@
 export const FETCH_CASES = 'FETCH_CASES';
 export const NEW_CASE = 'NEW_CASE';
 export const NEW_CASE_ERROR = 'NEW_CASE_ERROR';
+
+export const FETCH_PARTS = 'FETCH_PARTS';
+export const NEW_PART = 'NEW_PART';
+export const NEW_PART_ERROR = 'NEW_PART_ERROR';
