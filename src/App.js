43b87d95cde6dfdf81e5705ed615312a33c8024f
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-20 20:22:38 +0530

    did some styling for home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import './App.css';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -10,15 +11,26 @@ import Signup from './components/Signup'
 import CaseList from './components/caseList';
 import CaseForm from './components/caseForm';
 
+const AppDiv = styled.div`
+  background: #D3D8DA;
+  min-height: 100vh;
+`
+
+const Div = styled.div`
+  width: 100%;
+  height: 10vh;
+  background: #472C6C;
+`
 
 function App(props) {
 
   const { isAuthenticated, isVerifying } = props;
 
   return (
-    <div>
-      <CaseList/>      
-    </div>
+    <AppDiv>
+      <Div></Div>
+      <CaseList/>
+    </AppDiv>
     // <Switch>
     //   <div className="App">
     //     <ProtectedRoute 
--- src/components/caseList.js
+++ src/components/caseList.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
 import { fetchCases } from '../actions/caseActions';
 
+const ListDiv = styled.div`
+    width: 75%;
+    margin: auto;
+`
+
+const SkillDiv = styled.div`
+    box-sizing: border-box;
+    height: 80px;
+    background: #FFFFFF;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`
+
+const DotDiv = styled.div`
+    width: 10px;
+    height: 10px;
+    background: red;
+    border-radius: 50%;
+    margin-left: -30px;
+    /* margin-right: -30px; */
+`
+
 function CaseList(props) {
-    console.log(props)
+
     useEffect(() => {
         props.fetchCases();
     }, [])
@@ -12,17 +40,20 @@ function CaseList(props) {
     let caseItems
     if (props.cases) {
         caseItems = props.cases.map((item, key) => (
-            <div key={key}>
-                <h3>{item.skill}</h3>
+            <SkillDiv key={key}>
+                <DotDiv></DotDiv>
+                <p>{item.skill}</p>
                 <p>{item.created_at}</p>
-            </div>
+                <p>completion</p>
+                <p>*</p>
+            </SkillDiv>
         ))
     }
     return (
-        <div>
+        <ListDiv>
             <h1>Cases List</h1>
             {caseItems}
-        </div>
+        </ListDiv>
     )
 }
 
